4b3e42c3829433bceca6b29913de72006027653a
unknown <you@example.com>
2021-06-18 18:25:12 +0500

    class 14

---
+++ app.js
@@ -0,0 +1,184 @@
+// var a = new Date();
+// document.write(a)
+// alert(a)
+
+
+// var a =new Date();
+// var b = a.toString();
+// var c = b.slice(0,3)
+// alert (c)
+// alert (a)
+
+
+
+// var b = a.getDay();
+// alert (b)
+
+
+
+// var b = a.getTime()
+// alert(b)
+
+
+// var b = a.getDay()
+// var b = a.getMonth()
+// var b = a.getSeconds()
+// var b = a.getHours()
+// var b = a.getFullYear()
+
+// alert(b)
+
+
+
+// var a = new Date(prompt ("Enter you date of birth " , "Jan 1 , 1990"))
+// var b = a.getTime();
+// var c = new Date();
+// var v = c.getTime()
+// var dif =  v - b
+// var y = dif/ (1000*60*60*24*30*12)
+// var ac= Math.floor(y)
+// alert(ac)
+
+// var a= new Date()
+// a.setFullYear(2002)
+// alert(a)
+
+// function greeting (){
+//     alert("sjsjxcs")
+// }
+// greeting ();
+
+// function add (a,b,c){
+//     alert(a+b+c)
+// }
+// add(3,5,6)
+
+// function add (a,b,c){
+//     var f = 18;
+//     var z = a+b+c
+//     return z;
+// }
+
+// var g = add (7,9,6)
+// alert(g)
+// alert(f)
+
+// var h = 15 
+// function add (a,b,c){
+//     alert(h)
+//     // var f = 18;
+//     var z = a+b+c
+//     return z;
+// }
+
+// var g = add (7,9,6)
+// alert(g)
+
+
+
+// function age(){
+    
+// var a = new Date(prompt ("Enter you date of birth " , "Jan 1 , 1990"))
+// var b = a.getTime();
+// var c = new Date();
+// var v = c.getTime()
+// var dif =  v - b
+// var y = dif/ (1000*60*60*24*30*12)
+// var ac= Math.floor(y)
+// alert(ac)
+// }
+// age ();
+
+
+// function first (a,b){
+//     // alert(a+b)
+//     return b+a
+//  }
+
+// console.log(first (2,3))
+
+// console.log(first (13,7))
+
+
+// function first (a,b){
+//     // alert(a+b)
+//     return "hello"
+//  }
+
+// console.log(first (2,3))
+// console.log(first (13,7))
+
+
+// var a = "saimoon"
+// function first(){
+//      a = "ahmed"
+//  }
+// first();
+// console.log(a)
+
+
+
+// var a = "saimoon"
+// function first(){
+//      a = "ahmed"
+//  }
+// // first();
+// console.log(a)
+
+
+
+// function sa(num1,op,num2){
+//     if (op=== "+"){
+//         return num1+num2
+//     }
+//     else if (op=== "-"){
+//         return num1-num2
+//     }
+//     else if (op=== "*"){
+//         return num1*num2
+//     }
+//     else if (op=== "/"){
+//         return num1/num2
+//     }
+//     else{
+//         return "worng"
+//     }
+// }
+
+// var res= sa (5,"+",10)
+// var res1= sa (5,"-",10)
+// var res2= sa (5,"$",10)
+
+// console.log(res)
+// console.log(res1)
+// console.log(res2)
+
+
+
+
+// function foo (a,b = 3){
+//     return a+b
+// }
+// console.log(foo(1,5))
+
+
+
+// function foo (){
+//     // return a+b
+// }
+// console.log(foo())
+
+
+
+var na = "saimoon"
+switch (na){
+    case "ghous":
+    alert("hahaha")
+    break;
+    case "saimoon":
+        alert("ntnt")
+        break;
+        default:
+     alert("uwuyewey")
+     break;
+    }
